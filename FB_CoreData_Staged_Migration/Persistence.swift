ce5f8e795f9f2209a2c00eedf96f0da0c716c4cf
Axel Le Pennec <axel.le.pennec@gmail.com>
2024-02-23 18:23:46 +0100

    Initial Commit

--- FB_CoreData_Staged_Migration/Persistence.swift
+++ FB_CoreData_Staged_Migration/Persistence.swift
@@ -8,47 +8,76 @@ import CoreData
 struct PersistenceController {
     static let shared = PersistenceController()
 
-    static var preview: PersistenceController = {
-        let result = PersistenceController(inMemory: true)
-        let viewContext = result.container.viewContext
-        for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-        }
-        do {
-            try viewContext.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        }
-        return result
-    }()
-
     let container: NSPersistentContainer
 
-    init(inMemory: Bool = false) {
-        container = NSPersistentContainer(name: "FB_CoreData_Staged_Migration")
-        if inMemory {
-            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+    init() {
+        guard let modelURL: URL = Bundle.main.url(forResource: "Model", withExtension: "momd") else {
+            fatalError("Unable to find Model data model in the bundle.")
         }
+        
+        guard let coreDataModel: NSManagedObjectModel = NSManagedObjectModel(contentsOf: modelURL) else {
+            fatalError("Unable to create the Core Data model.")
+        }
+        
+        container = NSPersistentContainer(
+            name: "Database",
+            managedObjectModel: coreDataModel
+        )
+
+
+        let v1ModelChecksum: String = "43TdyQILlgOK3x7s2VFAVs76F26kHUYhsJ2kgXcen8Y="
+        
+        let v1ModelReference: NSManagedObjectModelReference = NSManagedObjectModelReference(
+            name: "Model",
+            in: Bundle.main,
+            versionChecksum: v1ModelChecksum
+        )
+        
+//        let v1ModelReference: NSManagedObjectModelReference = NSManagedObjectModelReference(
+//            model: coreDataModel,
+//            versionChecksum: v1ModelChecksum
+//        )
+//        
+//        let v1ModelReference: NSManagedObjectModelReference = NSManagedObjectModelReference(
+//            fileURL: modelURL,
+//            versionChecksum: v1ModelChecksum
+//        )
+        
+        
+        let v2ModelChecksum: String = "2IT0LVliuZ99UHyhh0CjbSPY6nzWtAy5tpseH8QWHuw="
+        
+        let v2ModelReference: NSManagedObjectModelReference = NSManagedObjectModelReference(
+            name: "Model 2",
+            in: Bundle.main,
+            versionChecksum: v2ModelChecksum
+        )
+        
+//        let v2ModelReference: NSManagedObjectModelReference = NSManagedObjectModelReference(
+//            fileURL: modelURL,
+//            versionChecksum: v2ModelChecksum
+//        )
+//        
+//        let v2ModelReference: NSManagedObjectModelReference = NSManagedObjectModelReference(
+//            model: coreDataModel,
+//            versionChecksum: v2ModelChecksum
+//        )
+                    
+        let customStage: NSCustomMigrationStage = NSCustomMigrationStage(
+            migratingFrom: v1ModelReference,
+            to: v2ModelReference
+        )
+
+        let migrationManager: NSStagedMigrationManager = NSStagedMigrationManager([customStage])
+        
+        let description = container.persistentStoreDescriptions.first
+        description?.setOption(migrationManager, forKey: NSPersistentStoreStagedMigrationManagerOptionKey)
+        
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
+        
         container.viewContext.automaticallyMergesChangesFromParent = true
     }
 }
