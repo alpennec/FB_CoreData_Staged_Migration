425e4e6f2db9d92139174056d0ad6c3a227cab38
Axel Le Pennec <axel.le.pennec@gmail.com>
2024-02-24 06:13:22 +0100

    Fixing the issue with the correct way to do it with Core Data

--- FB_CoreData_Staged_Migration/Persistence.swift
+++ FB_CoreData_Staged_Migration/Persistence.swift
@@ -25,42 +25,54 @@ struct PersistenceController {
         )
 
 
-        let v1ModelChecksum: String = "43TdyQILlgOK3x7s2VFAVs76F26kHUYhsJ2kgXcen8Y="
+        let modelV1URL: URL = modelURL.appending(component: "Model.mom")
+        guard let modelV1: NSManagedObjectModel = NSManagedObjectModel(contentsOf: modelV1URL) else {
+            fatalError("Unable to find Model data model in the bundle.")
+        }
+        
+        let v1ModelChecksum: String = modelV1.versionChecksum
+        print(v1ModelChecksum, "43TdyQILlgOK3x7s2VFAVs76F26kHUYhsJ2kgXcen8Y=") // same as the one from the VersionInfo.plist or build log
         
         let v1ModelReference: NSManagedObjectModelReference = NSManagedObjectModelReference(
-            name: "Model",
-            in: Bundle.main,
+            model: modelV1,
             versionChecksum: v1ModelChecksum
         )
         
 //        let v1ModelReference: NSManagedObjectModelReference = NSManagedObjectModelReference(
-//            model: coreDataModel,
+//            name: "Model",
+//            in: Bundle.main,
 //            versionChecksum: v1ModelChecksum
 //        )
-//        
+        
 //        let v1ModelReference: NSManagedObjectModelReference = NSManagedObjectModelReference(
 //            fileURL: modelURL,
 //            versionChecksum: v1ModelChecksum
 //        )
         
+        let modelV2URL: URL = modelURL.appending(component: "Model 2.mom")
+        guard let modelV2: NSManagedObjectModel = NSManagedObjectModel(contentsOf: modelV2URL) else {
+            fatalError("Unable to find Model data model in the bundle.")
+        }
         
-        let v2ModelChecksum: String = "2IT0LVliuZ99UHyhh0CjbSPY6nzWtAy5tpseH8QWHuw="
+        let v2ModelChecksum: String = modelV2.versionChecksum
+        print(v2ModelChecksum, "2IT0LVliuZ99UHyhh0CjbSPY6nzWtAy5tpseH8QWHuw=") // same as the one from the VersionInfo.plist or build log
         
         let v2ModelReference: NSManagedObjectModelReference = NSManagedObjectModelReference(
-            name: "Model 2",
-            in: Bundle.main,
+            model: modelV2,
             versionChecksum: v2ModelChecksum
         )
+
+//        let v2ModelReference: NSManagedObjectModelReference = NSManagedObjectModelReference(
+//            name: "Model 2",
+//            in: Bundle.main,
+//            versionChecksum: v2ModelChecksum
+//        )
         
 //        let v2ModelReference: NSManagedObjectModelReference = NSManagedObjectModelReference(
 //            fileURL: modelURL,
 //            versionChecksum: v2ModelChecksum
 //        )
-//        
-//        let v2ModelReference: NSManagedObjectModelReference = NSManagedObjectModelReference(
-//            model: coreDataModel,
-//            versionChecksum: v2ModelChecksum
-//        )
+        
                     
         let customStage: NSCustomMigrationStage = NSCustomMigrationStage(
             migratingFrom: v1ModelReference,
